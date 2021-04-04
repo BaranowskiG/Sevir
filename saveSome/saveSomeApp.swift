e49539cdc976f37afe7ba21f1bbc03e0b3d02a29
Grzegorz <workbaranowski@gmail.com>
2021-04-04 17:11:01 +0200

    create Xcode project

---
+++ saveSome/saveSomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  saveSomeApp.swift
+//  saveSome
+//
+//  Created by Grzegorz Baranowski on 04/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct saveSomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
